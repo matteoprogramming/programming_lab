c7b48f91f10a0aaea0fb5ba48b59a210e1642a9e
matteoprogramming <148125922+matteoprogramming@users.noreply.github.com>
2024-01-13 16:59:18 +0100

    Update 3_nov_1.py
    fix the logic error

--- 3_nov_2023/3_nov_1.py
+++ 3_nov_2023/3_nov_1.py
@@ -12,8 +12,6 @@ def isPrime(n):
     for i in range(3,int(n**0.5)+1,2):
         if n%i == 0:
             return False
-        else:
-            return True
     return True
 
 def find_max_multiples(n, numbers_list):
